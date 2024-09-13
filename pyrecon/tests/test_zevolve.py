16a29dcc8b9ab73676fae94e9c38b459b97a55a2
epaillas <enrique.paillas@uwaterloo.ca>
2024-09-13 14:22:39 -0700

    updated test

--- pyrecon/tests/test_zevolve.py
+++ pyrecon/tests/test_zevolve.py
@@ -1,4 +1,4 @@
-from pyrecon import IterativeFFTReconstruction, mpi, setup_logging
+from pyrecon import IterativeFFTReconstruction, setup_logging
 from pyrecon.utils import DistanceToRedshift, sky_to_cartesian, cartesian_to_sky
 import mpytools as mpy
 from cosmoprimo.fiducial import DESI
@@ -6,7 +6,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 import numpy as np
 import fitsio
 from pathlib import Path
-import matplotlib.pyplot as plt
+
 
 def read_data():
     data_dir = f'/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/{version}'
@@ -19,32 +19,55 @@ def read_randoms(idx=0):
     return fitsio.read(data_fn)
 
 def get_clustering_positions_weights(data):
-    ra = data['RA']
-    dec = data['DEC']
-    dist = distance(data['Z'])
+    mask = (data['Z'] > zmin) & (data['Z'] < zmax)
+    ra = data[mask]['RA']
+    dec = data[mask]['DEC']
+    dist = distance(data[mask]['Z'])
     pos = sky_to_cartesian(ra=ra, dec=dec, dist=dist)
-    weights = data['WEIGHT']
-    return pos, weights
+    weights = data[mask]['WEIGHT']
+    return pos, weights, mask
 
-def run_recon(recon_weights=False, fmesh=False):
-    if fmesh:
-        z = np.linspace(0.0, 5.0, 10000)
-        # f = np.ones_like(z) * 0.824
-        growth_at_dist = InterpolatedUnivariateSpline(distance(z), cosmo.growth_rate(z), k=3)
-        f = 'mesh'
+def bias_evolution(z, tracer='QSO'):
+    """
+    Bias model fitted from DR1 unblinded data (the formula from Laurent et al. 2016 (1705.04718))
+    """
+    if tracer == 'QSO':
+        alpha = 0.237
+        beta = 2.328
+    elif tracer == 'LRG':
+        alpha = 0.209
+        beta = 2.790
+    elif tracer == 'ELG_LOPnotqso':
+        alpha = 0.153 
+        beta = 1.541
     else:
-        growth_at_dist = None
-        f = growth_rate[tracer]
-    if recon_weights:
-        nz_dir = f'/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/{version}/'
-        nz_fn = Path(nz_dir) / f'{tracer}_{region}_nz.txt'
-        data = np.genfromtxt(nz_fn)
-        zmid = data[:, 0]
-        nz = data[:, 3]
-        n_at_dist = InterpolatedUnivariateSpline(distance(zmid), nz, k=3, ext=1)
+        raise NotImplementedError(f'{tracer} not implemented.')
+    return alpha * ((1+z)**2 - 6.565) + beta
+
+def interpolate_growth_bias():
+    z = np.linspace(0.0, 5.0, 10000)
+    growth_rate = cosmo.growth_rate(z)
+    bias = bias_evolution(z)
+    growth_at_dist = InterpolatedUnivariateSpline(distance(z), growth_rate, k=3)
+    bias_at_dist = InterpolatedUnivariateSpline(distance(z), bias, k=3)
+    return growth_at_dist, bias_at_dist
+
+def interpolate_number_density():
+    nz_dir = f'/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/v1.2/blinded/'
+    nz_fn = Path(nz_dir) / f'{tracer}_{region}_nz.txt'
+    data = np.genfromtxt(nz_fn)
+    zmid = data[:, 0]
+    nz = data[:, 3]
+    n_at_dist = InterpolatedUnivariateSpline(distance(zmid), nz, k=3, ext=1)
+    return n_at_dist
+
+def run_recon(recon_weights=False, fmesh=False, bmesh=False):
+    f = growth_at_dist if fmesh else growth_rate[tracer]
+    bias = bias_at_dist if bmesh else bias
     if mpicomm.rank == 0:
         data = read_data()
-        data_positions, data_weights = get_clustering_positions_weights(data)
+        data_positions, data_weights, data_mask = get_clustering_positions_weights(data)
+        data = data[data_mask]
     else:
         data_positions, data_weights = None, None
     recon = IterativeFFTReconstruction(f=f, bias=bias, positions=data_positions,
@@ -55,7 +78,7 @@ def run_recon(recon_weights=False, fmesh=False):
     for i in range(nrand):
         if mpicomm.rank == 0:
             randoms = read_randoms(i)
-            random_positions, random_weights = get_clustering_positions_weights(randoms)
+            random_positions, random_weights, randoms_mask = get_clustering_positions_weights(randoms)
         else:
             random_positions, random_weights = None, None
         recon.assign_randoms(random_positions, random_weights)
@@ -67,28 +90,31 @@ def run_recon(recon_weights=False, fmesh=False):
     if mpicomm.rank == 0:
         dist, ra, dec = cartesian_to_sky(data_positions_recon)
         data['RA'], data['DEC'], data['Z'] = ra, dec, d2r(dist)
-        if recon_weights and fmesh:
-            output_dir = '/pscratch/sd/e/epaillas/recon_weights/recon_evo_weights/'
+        if recon_weights and fmesh and bmesh:
+            output_dir = f'/pscratch/sd/e/epaillas/recon_weights/growth_bias_weights/{version}'
+        elif fmesh and bmesh:
+            output_dir = f'/pscratch/sd/e/epaillas/recon_weights/growth_bias/{version}'
         elif recon_weights:
-            output_dir = '/pscratch/sd/e/epaillas/recon_weights/recon_weights/'
+            output_dir = f'/pscratch/sd/e/epaillas/recon_weights/weights/{version}'
         elif fmesh:
-            output_dir = '/pscratch/sd/e/epaillas/recon_weights/recon_evo/'
+            output_dir = f'/pscratch/sd/e/epaillas/recon_weights/growth/{version}'
         else:
-            output_dir = '/pscratch/sd/e/epaillas/recon_weights/recon_vanilla/'
+            output_dir = f'/pscratch/sd/e/epaillas/recon_weights/vanilla/{version}'
         Path(output_dir).mkdir(parents=True, exist_ok=True)
-        output_fn = Path(output_dir) / f'{tracer}_{region}_recon.dat.fits'
+        output_fn = Path(output_dir) / f'{tracer}_{region}_clustering.IFTrecsym.dat.fits'
         fitsio.write(output_fn, data, clobber=True)
-    for i in range(nrand):
+    for i in range(4):
         if mpicomm.rank == 0:
             randoms = read_randoms(i)
-            randoms_positions, randoms_weights = get_clustering_positions_weights(randoms)
+            randoms_positions, randoms_weights, randoms_mask = get_clustering_positions_weights(randoms)
+            randoms = randoms[randoms_mask]
         else:
             randoms_positions, randoms_weights = None, None
         randoms_positions_recon = recon.read_shifted_positions(randoms_positions)
         if mpicomm.rank == 0:
             dist, ra, dec = cartesian_to_sky(randoms_positions_recon)
             randoms['RA'], randoms['DEC'], randoms['Z'] = ra, dec, d2r(dist)
-            output_fn = Path(output_dir) / f'{tracer}_{region}_{i}_recon.ran.fits'
+            output_fn = Path(output_dir) / f'{tracer}_{region}_{i}_clustering.IFTrecsym.ran.fits'
             fitsio.write(output_fn, randoms, clobber=True)
 
 setup_logging()
@@ -99,20 +125,25 @@ P0 = {'LRG': 8.9e3, 'QSO': 5.0e3}
 smoothing_radius = {'LRG': 15, 'QSO': 30}
 growth_rate = {'LRG': 0.834, 'QSO': 0.928}
 
-version = 'v1.2/blinded'
-tracer = 'LRG'
-region = 'NGC'
+version = 'v1.2/unblinded'
+tracer = 'QSO'
+regions = ['NGC', 'SGC']
+zmin, zmax = 0.8, 2.1
 bias = bias[tracer]
 P0 = P0[tracer]
 smoothing_radius = smoothing_radius[tracer]
-cellsize = 4.0
+cellsize = 5.0
 nrand = 18
 
 cosmo = DESI()
 distance = cosmo.comoving_radial_distance
 d2r = DistanceToRedshift(distance)
 
-run_recon(recon_weights=False, fmesh=False)
-run_recon(recon_weights=False, fmesh=True)
-run_recon(recon_weights=True, fmesh=True)
+growth_at_dist, bias_at_dist = interpolate_growth_bias()
 
+for region in regions:
+    n_at_dist = interpolate_number_density()
+
+    # run_recon(recon_weights=False, fmesh=False)
+    run_recon(recon_weights=False, fmesh=True, bmesh=True)
+    # run_recon(recon_weights=True, fmesh=True, bmesh=True)
